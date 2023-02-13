da9ab10e937c9930d3c0d0c18c027ef115e78124
dined1@mail.ru <dined1@mail.ru>
2023-02-14 00:02:21 +0900

    just a check

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,8 @@ app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
   const { rows } = await pool.query("SELECT tadoushi, jidoushi FROM tajiverb");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+  res.send(`Tadoushi - Jidoushi`);
+  res.send(`${rows[0].tadoushi} - ${rows[0].jidoushi}`);
 });
 
 app.listen(port, () => {
