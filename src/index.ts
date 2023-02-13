ca6874ebc89c76eb40b46c34266f80b24e4921ca
dined1@mail.ru <dined1@mail.ru>
2023-02-14 00:28:40 +0900

    just a check

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,23 @@ const pool = new pg.Pool();
 const app = express();
 const port = process.env.PORT || 3333;
 
+app.set('view engine', 'hbs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
+const hbs = require('hbs');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 app.get("/", async (req, res) => {
   const { rows } = await pool.query("SELECT tadoushi, jidoushi FROM tajiverb");
-  res.send(`${rows[0].tadoushi} - ${rows[0].jidoushi}`);
+//   res.send(`${rows[0].tadoushi} - ${rows[0].jidoushi}`);
+  res.render('ind', {
+      subject: 'hbs template engine',
+      name: 'our template',
+      link: 'https://google.com'
+    });
 });
 
 app.listen(port, () => {
